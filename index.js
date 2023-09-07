4e625b498e1c819e380cd9ef3bb603fd3128cc23
Abodunrin Abdulsalam <abdulsalamabodunrin369@gmail.com>
2023-09-07 22:15:19 +0100

    fix: refactor and utcnow

--- index.js
+++ index.js
@@ -5,31 +5,48 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+// Constants
+const DAYS_OF_WEEK = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const DEFAULT_SLACK_NAME = "elhakamy";
+const GITHUB_FILE_URL =
+  "https://github.com/abdulsalamcodes/hng-stage-one-task/blob/main/index.js";
+const GITHUB_REPO_URL = "https://github.com/abdulsalamcodes/hng-stage-one-task";
+
 app.get("/api", (req, res) => {
   try {
     const query = req.query;
-    const currentDate = new Date("2023-09-07T20:56:48Z");
+    const currentDate = new Date();
 
-    const daysOfWeek = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-    const dayOfWeek = daysOfWeek[currentDate.getDay()];
-    const utcTimeWithoutMilliseconds = currentDate.toISOString().split('.')[0] + 'Z';
+    const dayOfWeek = DAYS_OF_WEEK[currentDate.getDay()];
 
+    function getUTCDateString() {
+      const year = currentDate.getUTCFullYear();
+      const month = String(currentDate.getUTCMonth() + 1).padStart(2, "0"); // Months are zero-based
+      const day = String(currentDate.getUTCDate()).padStart(2, "0");
+      const hours = String(currentDate.getUTCHours()).padStart(2, "0");
+      const minutes = String(currentDate.getUTCMinutes()).padStart(2, "0");
+      const seconds = String(currentDate.getUTCSeconds()).padStart(2, "0");
+
+      return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z`;
+    }
+
+    const utcTimestamp = getUTCDateString();
 
     const responseBody = {
-      slack_name: query.slack_name || "elhakamy",
+      slack_name: query.slack_name || DEFAULT_SLACK_NAME,
       current_day: dayOfWeek,
-      utc_time: utcTimeWithoutMilliseconds,
+      utc_time: utcTimestamp,
       track: query.track || "Backend",
-      github_file_url: "https://github.com/abdulsalamcodes/hng-stage-one-task/blob/main/index.js",
-      github_repo_url: "https://github.com/abdulsalamcodes/hng-stage-one-task",
+      github_file_url: GITHUB_FILE_URL,
+      github_repo_url: GITHUB_REPO_URL,
       status_code: 200,
     };
 
